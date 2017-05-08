7f99f52d97138503fa97ee58c3b48bd43d31d04b
John Peck <john_m_peck@yahoo.com>
2017-05-07 21:40:51 -0400

    Add project files.

---
+++ John.ExerciseCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace John.ExerciseCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
